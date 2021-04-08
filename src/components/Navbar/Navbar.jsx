c3b0eabcc85c451f786d9fdcfed451e67a60b8fb
Proltroll_phone <vladimir_borisof@mail.ru>
2021-04-08 14:21:40 +0300

    v.0.0.1 изменения в мобильной верстке

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,36 +1,50 @@
-import { NavLink } from 'react-router-dom';
+import {
+  NavLink
+} from 'react-router-dom';
 import cl from './Navbar.module.css'
 
+
+
+
 const Navbar = () => {
+   
+    function press(e) {
+        alert(e);
+    };
+  
   return (
-    <nav className={cl.nav} >
-      <div className={cl.item} >
+    <nav className={cl.nav} onClick={press}>
+      <div className={cl.empty}>
+    </div>
+      <div className={cl.itemList}>
+      <div className={cl.item}>
         <NavLink to="/profile" activeClassName={cl.active}>
           Profile
           </NavLink>
       </div>
-      <div className={cl.item} >
+      <div className={cl.item}>
         <NavLink to="/dialogs" activeClassName={cl.active}>
           Dialogs
           </NavLink>
       </div>
-      <div className={cl.item} >
+      <div className={cl.item}>
         <NavLink to="/news" activeClassName={cl.active}>
           News
           </NavLink>
       </div>
-      <div className={cl.item} >
+      <div className={cl.item}>
 
         <NavLink to="/music" activeClassName={cl.active}>
           Music
         </NavLink>
       </div>
-      <div className={cl.item} >
+      <div className={cl.item}>
 
         <NavLink to="/settings" activeClassName={cl.active}>
           Settings
         </NavLink>
       </div>
+    </div>
     </nav>
   );
 }
